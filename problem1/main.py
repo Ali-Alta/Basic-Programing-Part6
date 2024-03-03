8c6d082483c0dfdf4462865dce23e859c907ec6b
alisofyansaputra <Buffware4@gmail.com>
2024-03-03 15:58:11 +0700

    problem1.main.py

--- problem1/main.py
+++ problem1/main.py
@@ -1,6 +1,16 @@
 def compare(a, b):
-    pattern = ""
-    return pattern
+    len_a, len_b = len(a), len(b)
+    result = ""
+
+    for i in range(len_a):
+        for j in range(len_b):
+            k=0
+            while i + k < len_a and j + k < len_b and a[i + k] == b[j + k]:
+                k += 1
+            
+            if k > len(result):
+                result = a[i:i + k]
+    return result 
 
 if __name__ == '__main__':
     print(compare("AKA", "AKASHI")) # AKA
